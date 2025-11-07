a4d1b79e4ba9b197b6362f647bde9b0af66ce7b5
Ghanshyam Kumar <rajaryan0958@gmail.com>
2025-11-07 22:08:45 +0530

    Create gem-terraform-firestore-create.sh

---
+++ gem-terraform-firestore-create.sh
@@ -0,0 +1,77 @@
+PROJECT_ID=$(gcloud config get-value project)
+
+gcloud config set project "$PROJECT_ID"
+
+gcloud services enable firestore.googleapis.com
+
+gcloud services enable cloudbuild.googleapis.com
+
+gcloud storage buckets create gs://"$PROJECT_ID"-tf-state --location=us
+
+cat > main.tf <<EOF
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+  backend "gcs" {
+    bucket = ""PROJECT_ID"-tf-state"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = ""PROJECT_ID""
+  region  = ""REGION""
+}
+
+resource "google_firestore_database" "default" {
+  name     = "default"
+  project  = ""PROJECT_ID""
+  location_id = "nam5"
+  type     = "FIRESTORE_NATIVE"
+}
+
+output "firestore_database_name" {
+  value       = google_firestore_database.default.name
+  description = "The name of the Cloud Firestore database."
+}
+EOF
+
+cat > variables.tf <<EOF
+variable "project_id" {
+  type        = string
+  description = "The ID of the Google Cloud project."
+  default     = ""PROJECT_ID""
+}
+
+variable "bucket_name" {
+  type        = string
+  description = "Bucket name for terraform state"
+  default     = ""PROJECT_ID"-tf-state"
+}
+EOF
+
+cat > outputs.tf <<EOF
+output "project_id" {
+  value       = var.project_id
+  description = "The ID of the Google Cloud project."
+}
+
+output "bucket_name" {
+  value       = var.bucket_name
+  description = "The name of the bucket to store terraform state."
+}
+EOF
+
+terraform init
+
+terraform plan
+
+terraform apply -auto-approve
+
+terraform destroy
+
+echo "🥳Your Lab is Completed."
