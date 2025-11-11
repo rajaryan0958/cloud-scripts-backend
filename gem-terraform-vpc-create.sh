55cf14ca2e8ea876d5bee8a3b8b36e638b12bcea
Ghanshyam Kumar <rajaryan0958@gmail.com>
2025-11-11 22:52:50 +0530

    Create gem-terraform-vpc-create.sh

---
+++ gem-terraform-vpc-create.sh
@@ -0,0 +1,99 @@
+PROJECT_ID=$(gcloud config get-value project)
+
+gcloud config set project "$PROJECT_ID"
+
+read -p "Enter your Region (e.g., us-east1, us-west1, etc.): " REGION
+
+gcloud config set compute/region "$REGION"
+
+read -p "Enter your Region (e.g., us-east1-a, us-west1-b, etc.): " ZONE
+
+gcloud config set compute/zone "$ZONE"
+
+gcloud storage buckets create gs://$PROJECT_ID-terraform-state --project=$PROJECT_ID --location=us
+
+gcloud services enable cloudresourcemanager.googleapis.com --project=$PROJECT_ID
+
+cat > main.tf <<EOF
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
+  }
+  backend "gcs" {
+    bucket = ""PROJECT_ID"-terraform-state"
+    prefix = "terraform/state"
+  }
+}
+
+provider "google" {
+  project = ""PROJECT_ID""
+  region  = ""REGION""
+}
+
+resource "google_compute_network" "vpc_network" {
+  name                    = "custom-vpc-network"
+  auto_create_subnetworks = false
+}
+
+resource "google_compute_subnetwork" "subnet_us" {
+  name            = "subnet-us"
+  ip_cidr_range   = "10.10.1.0/24"
+  region          = ""REGION""
+  network         = google_compute_network.vpc_network.id
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc_network.name
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow_icmp" {
+  name    = "allow-icmp"
+  network = google_compute_network.vpc_network.name
+
+  allow {
+    protocol = "icmp"
+  }
+  source_ranges = ["0.0.0.0/0"]
+}
+EOF
+
+cat > variables.tf <<EOF
+variable "project_id" {
+  type        = string
+  description = "The ID of the Google Cloud project"
+  default     = ""PROJECT_ID""
+}
+
+variable "region" {
+  type        = string
+  description = "The region to deploy resources in"
+  default     = ""REGION""
+}
+EOF
+
+cat > outputs.tf <<EOF
+output "network_name" {
+  value       = google_compute_network.vpc_network.name
+  description = "The name of the VPC network"
+}
+
+output "subnet_name" {
+  value       = google_compute_subnetwork.subnet_us.name
+  description = "The name of the subnetwork"
+}
+EOF
+
+terraform init
+
+terraform plan
+
+terraform apply --auto-approve
