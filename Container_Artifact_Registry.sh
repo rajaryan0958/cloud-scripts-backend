91b4a78933cdebe4d917f8f2a0566dd7b1d3a11f
Ghanshyam Kumar <rajaryan0958@gmail.com>
2025-11-11 23:45:01 +0530

    Update Container_Artifact_Registry.sh

--- Container_Artifact_Registry.sh
+++ Container_Artifact_Registry.sh
@@ -9,12 +9,6 @@ read -p "Enter your Region (e.g., us-east1, us-west1, etc.): " REGION
 gcloud config set project "$PROJECT_ID"
 gcloud config set run/region "$REGION"
 
-
-# Display what was set
-echo "✅ Project ID automatically set to: $PROJECT_ID"
-echo "✅ Cloud Run region automatically set to: $REGION"
-
-
 gcloud artifacts repositories create my-docker-repo \
     --repository-format=docker \
     --location=$REGION \
